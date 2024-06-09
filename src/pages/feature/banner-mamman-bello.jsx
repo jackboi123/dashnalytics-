b65550bde7b4bb90d9f89574d5ac7815428e94f4
Vershima Iorliam <jackiorliam90@gmail.com>
2024-06-09 09:26:44 +0100

    created the category page feature/banner-mamman-bello

---
+++ src/pages/feature/banner-mamman-bello.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function category() {
+  return (
+    <main style={{border:'1px solid red',padding:'15px',backgroundColor:'white'}}>
+        <nav style={{border:'1px solid red',padding:'15px',display:'flex',justifyContent:'space-between',backgroundColor:'lightgrey'}}>
+            <ul>
+                <li style={{margin:'0 15px',color:'darkblue'}}></li>
+                <li style={{margin:'0 15px',color:'darkblue'}}></li>
+                <li style={{margin:'0 15px',color:'darkblue'}}></li>                
+            </ul>
+        </nav>
+    </main>
+  )
+}
