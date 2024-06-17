04e5a309b793c976cde36c8a013c08afc28b1ced
Vershima Iorliam <jackiorliam90@gmail.com>
2024-06-17 16:38:46 +0100

    added the items to my category page

--- src/pages/feature/banner-mamman-bello.jsx
+++ src/pages/feature/banner-mamman-bello.jsx
@@ -5,9 +5,9 @@ export default function category() {
     <main style={{border:'1px solid red',padding:'15px',backgroundColor:'white'}}>
         <nav style={{border:'1px solid red',padding:'15px',display:'flex',justifyContent:'space-between',backgroundColor:'lightgrey'}}>
             <ul>
-                <li style={{margin:'0 15px',color:'darkblue'}}></li>
-                <li style={{margin:'0 15px',color:'darkblue'}}></li>
-                <li style={{margin:'0 15px',color:'darkblue'}}></li>                
+                <li style={{margin:'0 15px',color:'darkblue'}}>Pro Boost Shoe</li>
+                <li style={{margin:'0 15px',color:'darkblue'}}>Adidas Men Polo</li>
+                <li style={{margin:'0 15px',color:'darkblue'}}>Versace Golden Necklace</li>                
             </ul>
         </nav>
     </main>
